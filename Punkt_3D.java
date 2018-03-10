eecd056a181036daefa5f29685ea1b2ec40fc023
AKakuba <adamkakuba96@gmail.com>
2018-03-10 17:43:10 +0100

    Klasa 3D

---
+++ Punkt_3D.java
@@ -0,0 +1,22 @@
+package punkty;
+
+public class Punkt_3D extends Punkt_2D{
+	
+	public int z;
+	
+	public Punkt_3D() {
+		
+		x=0;
+		y=0;
+		z=0;
+	}
+	
+	public Punkt_3D(int a,int b,int c) {
+		
+		this.x=a;
+		this.y=b;
+		this.z=c;
+		
+	}
+	
+}
