7fe23aad5a7e11df5d1078a9780e3376567bf799
AKakuba <adamkakuba96@gmail.com>
2018-03-10 17:42:32 +0100

    Klasa 2D

---
+++ Punkt_2D.java
@@ -0,0 +1,19 @@
+package punkty;
+
+public class Punkt_2D extends Punkt_1D{
+	
+	public int y;
+	
+	public Punkt_2D() {
+
+		x=0;
+		y=0;
+	}
+	 
+	public Punkt_2D(int a,int b) {
+		
+		this.x=a;
+		this.y=b;
+		
+	}
+}
